09a7f6c17c797c493f4024059c7dd08d79e59c4c
Anna Taberski <aftaberski@gmail.com>
2016-10-26 15:11:28 -0400

    Version 0.1.6

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.clojars.quartet/clj-cron-parse "0.1.6-SNAPSHOT"
+(defproject org.clojars.quartet/clj-cron-parse "0.1.6"
   :description "A Clojure library for using cron expressions"
   :url "https://github.com/shmish111/clj-cron-parse"
   :license {:name "Eclipse Public License"
